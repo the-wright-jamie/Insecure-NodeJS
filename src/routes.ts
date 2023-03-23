6a908698396bb77fa2b828408fe0b1e17d013e74
Jamie Wright <jwright3478@gmail.com>
2023-03-23 08:59:58 +0000

    move show off up

--- src/routes.ts
+++ src/routes.ts
@@ -12,7 +12,7 @@ const maltracker = new Maltrack();
 
 app.use(express.json());
 
-http.createServer(app).listen(process.env.HOST_PORT, () => {
+http.createServer(app).listen(4000, () => {
   console.log(`The Best API Server has started successfully.`);
 });
 
@@ -23,12 +23,6 @@ app.route(`/getInsecure/user/:username`).get(async (req, res) => {
   } else {
     res.send(response);
   }
-});
-
-app.route(`/showOff/stringParsing/:string`).get(async (req, res) => {
-  res.send(
-    `SELECT * FROM insecurenode.users WHERE username = '${req.params.string}'`
-  );
 });
 
 app.route(`/getSecure/user/:username`).get(async (req, res) => {
@@ -43,6 +37,12 @@ app.route(`/getSecure/user/:username`).get(async (req, res) => {
 app.route(`/getInsecure/ip-address-logger`).get(async (req, res) => {
   const response = await maltracker.logIPAddress(req);
   res.status(400).send("Bad username or password");
+});
+
+app.route(`/showOff/stringParsing/:string`).get(async (req, res) => {
+  res.send(
+    `SELECT * FROM insecurenode.users WHERE username = '${req.params.string}' AND password ...`
+  );
 });
 
 app.all("*", function (req, res) {
